3caf77bca30c08beca26a5765952dbf1353fbff4
Biraj-Scholar <103089978+Biraj-Scholar@users.noreply.github.com>
2022-08-23 10:06:53 +0530

    Adding REST Service

---
+++ HelloWorld/src/demo/Game.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Game {
+	
+	private String GameName;
+	private int NumberOfPlayers;
+	private int Age;
+	private String Description;
+	private String Rules;
+	private String DemoVideo;
+	private int EstTime;
+	private String Help;
+	private String FitnessLevel;
+	private String Dificulty;
+
+}
